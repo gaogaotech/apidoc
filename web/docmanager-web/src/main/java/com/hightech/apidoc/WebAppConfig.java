3455ed355c01cdb7f7d8fed755d3af4e076e9954
pancz <zjpancz@163.com>
2017-06-28 13:41:56 +0800

    1、去除版权出错

--- web/docmanager-web/src/main/java/com/hightech/apidoc/WebAppConfig.java
+++ web/docmanager-web/src/main/java/com/hightech/apidoc/WebAppConfig.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2017 Zhongan.com All right reserved. This software is the
- * confidential and proprietary information of Zhongan.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Zhongan.com.
- */
 package com.hightech.apidoc;
 
 import java.util.List;
